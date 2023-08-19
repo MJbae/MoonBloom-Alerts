6ef6a214f8f0e55293002d08cc859ea20d87dbfd
배만진 <gentlemmj@gmail.com>
2023-08-19 10:57:22 +0900

    init

---
+++ src/main/kotlin/com/check/MoonBloom/Alerts/MoonBloomAlertsApplication.kt
@@ -0,0 +1,11 @@
+package com.check.MoonBloom.Alerts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoonBloomAlertsApplication
+
+fun main(args: Array<String>) {
+	runApplication<MoonBloomAlertsApplication>(*args)
+}
---
+++ src/test/kotlin/com/check/MoonBloom/Alerts/MoonBloomAlertsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.check.MoonBloom.Alerts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoonBloomAlertsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
