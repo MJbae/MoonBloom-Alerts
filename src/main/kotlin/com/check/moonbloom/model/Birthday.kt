faad5b6e0f57ab0d3803538d006b850e514746fc
배만진 <gentlemmj@gmail.com>
2023-08-19 20:00:06 +0900

    feat: 음력 계산 라이브러리 적용 및 생년월일 선택 기준일 수정

--- src/main/kotlin/com/check/moonbloom/model/Birthday.kt
+++ src/main/kotlin/com/check/moonbloom/model/Birthday.kt
@@ -1,16 +1,24 @@
 package com.check.moonbloom.model
 
+import com.github.usingsky.calendar.KoreanLunarCalendar
 import java.time.LocalDate
 
+
 class Birthday(private val date: LocalDate) {
-    fun lunar(): LocalDate = lunarToGregorian(thisBirthday(date))
-    fun gregorian(): LocalDate = thisBirthday(date)
+    private var lunarConverter = KoreanLunarCalendar.getInstance()
+
+    fun lunar(): LocalDate = thisBirthday(date)
+    fun gregorian(): LocalDate = lunarToGregorian(thisBirthday(date))
+
+
+    private fun thisBirthday(originalDate: LocalDate) =
+        LocalDate.of(LocalDate.now().year, originalDate.month, originalDate.dayOfMonth)
+
+    private fun lunarToGregorian(date: LocalDate): LocalDate {
+        lunarConverter.setLunarDate(LocalDate.now().year, date.month.value, date.dayOfMonth, false)
+        return LocalDate.of(lunarConverter.solarYear, lunarConverter.solarMonth, lunarConverter.solarDay)
+    }
 }
-
-private fun thisBirthday(originalDate: LocalDate) =
-    LocalDate.of(LocalDate.now().year, originalDate.month, originalDate.dayOfMonth)
-
-private fun lunarToGregorian(date: LocalDate) = date.plusDays(29)
 
 
 enum class CalendarType {
