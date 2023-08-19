0e5563ac4142ffeb757ae62d14bebe90eb9df534
배만진 <gentlemmj@gmail.com>
2023-08-19 21:31:25 +0900

    temp: DB 일시 비활성화

--- src/main/kotlin/com/check/moonbloom/MoonBloomAlertsApplication.kt
+++ src/main/kotlin/com/check/moonbloom/MoonBloomAlertsApplication.kt
@@ -1,11 +1,20 @@
 package com.check.moonbloom
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = [
+        DataSourceAutoConfiguration::class,
+        DataSourceTransactionManagerAutoConfiguration::class,
+        HibernateJpaAutoConfiguration::class
+    ]
+)
 class MoonBloomAlertsApplication
 
 fun main(args: Array<String>) {
-	runApplication<MoonBloomAlertsApplication>(*args)
+    runApplication<MoonBloomAlertsApplication>(*args)
 }
