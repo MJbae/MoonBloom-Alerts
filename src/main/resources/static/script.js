5007aae8c6da5366d5f5e9041cd2badb6b7a88f5
배만진 <gentlemmj@gmail.com>
2023-08-20 11:19:51 +0900

    feat: 전화번호 입력 필드 추가

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,8 +1,19 @@
+function isValidPhoneNumber(phone) {
+    const pattern = /^01[016789]-\d{3,4}-\d{4}$/;
+    return pattern.test(phone);
+}
+
 function fetchMessage() {
     let dob = document.getElementById("dob").value;
     let calendarType = document.getElementById("calendarType").value;
     let relationship = document.getElementById("relationship").value;
     let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+
+    if (!dob || !calendarType || !relationship || !isValidPhoneNumber(phone)) {
+        alert("모든 필수 입력 사항을 완료해주세요!");
+        return;
+    }
 
     fetch('/api/notifications', {
         method: 'POST',
