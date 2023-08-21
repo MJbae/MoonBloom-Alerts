7a3137e805b2f2372affc41b35e4632ed0600ad7
배만진 <gentlemmj@gmail.com>
2023-08-21 10:24:39 +0900

    fix: change mysql client for springboot3

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.github.usingsky:KoreanLunarCalendar:0.3.1")
-    implementation("mysql:mysql-connector-java:8.0.28")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
