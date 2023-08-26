ed7d6efdea38fb1d26e29bac3eb5f6b24c1d68e2
배만진 <gentlemmj@gmail.com>
2023-08-26 11:12:29 +0900

    style: prettier 스타일 포맷팅

--- src/main/resources/static/resultScript.js
+++ src/main/resources/static/resultScript.js
@@ -1,8 +1,9 @@
-window.onload = function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const message = urlParams.get('msg');
-    const phoneNo = urlParams.get('phoneNo');
+window.onload = function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const message = urlParams.get("msg");
+  const phoneNo = urlParams.get("phoneNo");
 
-    document.getElementById("fetchMsg").textContent = decodeURIComponent(message);
-    document.getElementById("fetchPhoneNumber").textContent = decodeURIComponent(phoneNo);
-}
+  document.getElementById("fetchMsg").textContent = decodeURIComponent(message);
+  document.getElementById("fetchPhoneNumber").textContent =
+    decodeURIComponent(phoneNo);
+};
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,56 +1,58 @@
 function isValidPhoneNumber(phoneNo) {
-    const pattern = /^01[016789]\d{6,8}$/;
-    return pattern.test(phoneNo);
+  const pattern = /^01[016789]\d{6,8}$/;
+  return pattern.test(phoneNo);
 }
 
 function checkMandatoryFields() {
-    let dob = document.getElementById("dob").value;
-    let relationship = document.getElementById("relationship").value;
-    let phoneNo = document.getElementById("phoneNo").value;
-    let name = document.getElementById("name").value;
+  let dob = document.getElementById("dob").value;
+  let relationship = document.getElementById("relationship").value;
+  let phoneNo = document.getElementById("phoneNo").value;
+  let name = document.getElementById("name").value;
 
-    const button = document.querySelector("button");
-    if (dob && relationship && name && isValidPhoneNumber(phoneNo)) {
-        button.disabled = false;
-    } else {
-        button.disabled = true;
-    }
+  const button = document.querySelector("button");
+  if (dob && relationship && name && isValidPhoneNumber(phoneNo)) {
+    button.disabled = false;
+  } else {
+    button.disabled = true;
+  }
 }
 
-["dob", "relationship", "phoneNo", "name"].forEach(id => {
-    document.getElementById(id).addEventListener('input', checkMandatoryFields);
+["dob", "relationship", "phoneNo", "name"].forEach((id) => {
+  document.getElementById(id).addEventListener("input", checkMandatoryFields);
 });
 
 document.querySelector("button").disabled = true;
 
 function fetchMessage() {
-    let dob = document.getElementById("dob").value;
-    let relationship = document.getElementById("relationship").value;
-    let phoneNo = document.getElementById("phoneNo").value;
-    let name = document.getElementById("name").value;
+  let dob = document.getElementById("dob").value;
+  let relationship = document.getElementById("relationship").value;
+  let phoneNo = document.getElementById("phoneNo").value;
+  let name = document.getElementById("name").value;
 
-    if (!dob || !relationship || !name || !isValidPhoneNumber(phoneNo)) {
-        alert("모든 필수 입력 사항을 완료해주세요!");
-        return;
-    }
+  if (!dob || !relationship || !name || !isValidPhoneNumber(phoneNo)) {
+    alert("모든 필수 입력 사항을 완료해주세요!");
+    return;
+  }
 
-    fetch('/api/notifications', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            dob: dob,
-            relationship: relationship,
-            name: name,
-            phoneNo: phoneNo
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        window.location.href = `result.html?msg=${encodeURIComponent(data.msg)}&phoneNo=${encodeURIComponent(data.phoneNo)}`;
+  fetch("/api/notifications", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      dob: dob,
+      relationship: relationship,
+      name: name,
+      phoneNo: phoneNo,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      window.location.href = `result.html?msg=${encodeURIComponent(
+        data.msg
+      )}&phoneNo=${encodeURIComponent(data.phoneNo)}`;
     })
     .catch((error) => {
-        console.error('Error:', error);
+      console.error("Error:", error);
     });
 }
